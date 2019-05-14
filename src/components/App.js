bf9d2f5a18de462073e227ecf11d67be006fe9d3
leewoojin <dldndldms@gmail.com>
2019-05-14 21:56:15 +0900

    MyInfo

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,35 @@ import Content from './Content';
 import { BrowserRouter as Router , Route, Link} from 'react-router-dom';
 import styled, {css} from 'styled-components';
 import NavContainer from '../containers/NavContainer';
+import MyInfo from './MyInfo';
 
 const Wrapper = styled.div`
   position: fixed;
   width: 100%;
-  z-index: 9999;
+  z-index: 8888;
   top: 0;
 `;
 
+
 class App extends Component {
   
+  state = {
+    show: true
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        show: false
+      })
+    }, 10000);
+  }
+
   render() {
     return (
       <div className="App">
         <Router>
+          {this.state.show ? <MyInfo /> : null }
           <Wrapper>
             <HeaderContainer />
             <NavContainer />
